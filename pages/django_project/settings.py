52003929461711fbaf89588885656291e11e343c
blktheta <blktheta@gmail.com>
2023-07-07 00:11:06 +0200

    Add fly.io deployment

--- pages/django_project/settings.py
+++ pages/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9y$*h(i)qe+lkhbo#n&-rms9a#$=rkml1x*nd@l#qq+@(@^6)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
